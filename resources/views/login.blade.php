3d19e6c7111cbbaa4afd993bf8692291fafe07b4
hwpn <youngartis.ru@gmail.com>
2023-08-30 16:03:39 +0630

    add

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,21 +1,21 @@
 @extends('layouts.master')
-
-
+@section('title', 'Login Page')
 @section('content')
     <div class="login-form">
-        <form action="{{route('login')}}" method="post">
+        <form action="{{ route('login') }}" method="post">
             @csrf
             <div class="form-group">
                 <label>Email Address @error('email')
-                    <small class="text-danger">{{$message}}</small>
-                @enderror</label>
+                        <small class="text-danger">{{ $message }}</small>
+                    @enderror
+                </label>
                 <input class="au-input au-input--full" type="email" name="email" placeholder="Email">
             </div>
             <div class="form-group">
-                <label>Password 
-                    <label>Email Address @error('password')
-                        <small class="text-danger">{{$message}}</small>
-                    @enderror</label>
+                <label>Password @error('password')
+                        <small class="text-danger">{{ $message }}</small>
+                    @enderror
+                </label>
                 <input class="au-input au-input--full" type="password" name="password" placeholder="Password">
             </div>
 
@@ -25,7 +25,7 @@
         <div class="register-link">
             <p>
                 Don't you have account?
-                <a href="{{route('auth#registerPage')}}">Sign Up Here</a>
+                <a href="{{ route('auth#registerPage') }}">Sign Up Here</a>
             </p>
         </div>
     </div>
